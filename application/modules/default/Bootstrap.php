caf4b37742a701b3361a992b1d8207d70f2066af
Bad-Hack <thekingofall@rocketmail.com>
2012-06-06 16:42:41 +0530

    Adding the modular support for the zend framework

---
+++ application/modules/default/Bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+class Default_Bootstrap extends Zend_Application_Module_Bootstrap {}
--- application/modules/default/controllers/ErrorController.php
+++ application/modules/default/controllers/ErrorController.php
@@ -1,6 +1,6 @@
 <?php
 
-class ErrorController extends Zend_Controller_Action {
+class Default_ErrorController extends Zend_Controller_Action {
 	
 	public function errorAction() {
 		$errors = $this->_getParam ( 'error_handler' );
