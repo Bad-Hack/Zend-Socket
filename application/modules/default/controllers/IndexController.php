ac44c875c0f504487aaa0ee663b68ac937768d9c
Bad-Hack <thekingofall@rocketmail.com>
2012-07-05 13:05:37 +0530

    Launching the core framework setup with Dojo Support and Sockets with stable dispatcher. Functionality enhancements would be implemented soon

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -5,12 +5,5 @@ class Default_IndexController extends Zend_Controller_Action {
 	public function init() {
 		$this->view->dojo()->enable();
 		/* Initialize action controller here */
-	
-	}
-	
-	public function indexAction() {
-		$this->view->form = new Default_Form_Index_Dojo();
-	}
-	public function testingAction() {
 	}
 }
--- application/modules/socket/Bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-class Socket_Bootstrap extends Zend_Application_Module_Bootstrap {
-}
