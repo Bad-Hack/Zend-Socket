05005930a265b96d044a4a85d76c8005fb57a415
Bad-Hack <thekingofall@rocketmail.com>
2012-07-06 20:00:23 +0530

    Testing Dojo

--- application/modules/default/forms/Index/Dojo.php
+++ application/modules/default/forms/Index/Dojo.php
@@ -1,14 +1,30 @@
 <?php
 class Default_Form_Index_Dojo extends Zend_Dojo_Form {
+	
 	public function init() {
-		
 		$this->setMethod ( 'post' );
 		$this->setName ( 'dojosample' );
-		$this->addElement ( 'DateTextBox', 'datetext', array (
-				'label' => 'Date:',
-				'required' => true,
-				'invalidMessage' => 'Invalid date specified.',
+		$this->setAttribs(array(
+				'onsubmit' => 'javascript:return false;'
+		));
+		$this->log = $this->createElement ( 'SimpleTextarea', 'log', array (
+				'label' => false,
+				'readonly'=> 'readonly',
 				'formatLength' => 'long' 
 		) );
+		
+		$this->message = $this->createElement('ValidationTextBox', 'message',array(
+				'label'	=> false,
+				'required'	=> true,
+				'trim'	=> true,
+				'invalidMessage' => 'Invalid Amount',
+				'focusOnLoad'        => true,
+				'placeholder'	=> 'Enter Chat Message'
+		));
+		$this->addElements(array(
+				$this->log,
+				$this->message
+		));
 	}
+
 }
