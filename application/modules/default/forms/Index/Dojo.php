6c815d99a017f91efbe7fc010ce46ac88baecf28
Bad-Hack <thekingofall@rocketmail.com>
2012-07-21 09:54:56 +0530

    Some Dojo Changes

--- application/modules/default/forms/Index/Dojo.php
+++ application/modules/default/forms/Index/Dojo.php
@@ -7,11 +7,12 @@ class Default_Form_Index_Dojo extends Zend_Dojo_Form {
 		$this->setAttribs(array(
 				'onsubmit' => 'javascript:return false;'
 		));
-		$this->log = $this->createElement ( 'SimpleTextarea', 'log', array (
+		/*$this->log = $this->createElement ( 'SimpleTextarea', 'log', array (
 				'label' => false,
 				'readonly'=> 'readonly',
 				'formatLength' => 'long' 
 		) );
+		*/
 		
 		$this->message = $this->createElement('ValidationTextBox', 'message',array(
 				'label'	=> false,
@@ -22,7 +23,7 @@ class Default_Form_Index_Dojo extends Zend_Dojo_Form {
 				'placeholder'	=> 'Enter Chat Message'
 		));
 		$this->addElements(array(
-				$this->log,
+				//$this->log,
 				$this->message
 		));
 	}
